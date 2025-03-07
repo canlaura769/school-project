0b4be535245cb61db799f93c44cc7c0227db1d8f
canlaura769 <canlaura769@gmail.com>
2025-03-07 21:37:46 +0000

    Add 2.ts

---
+++ src/2.ts
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+}
