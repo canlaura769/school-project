44be5c5a48671b254a990aea6206588242d5963d
canlaura769 <canlaura769@gmail.com>
2025-03-08 05:14:34 +0000

    Add 3.ts

---
+++ src/3.ts
@@ -0,0 +1,8 @@
+// TypeScript code goes here
+
+function getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const randomNumber = getRandomNumber(1, 10);
+console.log(randomNumber);
