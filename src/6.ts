6128363c9e61fdad67609567e183ecb8352df48c
canlaura769 <canlaura769@gmail.com>
2025-03-11 04:26:39 +0000

    Add 6.ts

---
+++ src/6.ts
@@ -0,0 +1,7 @@
+// TypeScript code goes here
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+let x = getRandomInt(5);
+console.log(`Random integer between 0 and 4: ${x}`);
