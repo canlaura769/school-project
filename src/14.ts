605f8954cb7451340f0c00a52fdf74281601839b
canlaura769 <canlaura769@gmail.com>
2025-03-19 04:59:39 +0000

    Add 14.ts

---
+++ src/14.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
