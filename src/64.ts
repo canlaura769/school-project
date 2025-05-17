64ec1f1d89cbcf1b1e498be3757c3220610f9c25
canlaura769 <canlaura769@gmail.com>
2025-05-17 08:21:37 +0000

    Add 64.ts

---
+++ src/64.ts
@@ -0,0 +1,11 @@
+import { sqrt } from 'mathjs';
+
+function isPrime(num: number): boolean {
+  if (num <= 1) return false;
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+console.log(isPrime(7));
